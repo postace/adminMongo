d305cb46f68a60890bef27e04f3bfe5739890966
Hai Pham <hai.phamthanh@vndirect.com.vn>
2018-01-10 18:25:49 +0700

    Format collection message, only used for XMPP message chat

--- public/js/adminMongo.js
+++ public/js/adminMongo.js
@@ -469,17 +469,26 @@ function paginate(){
         // clear the div first
         $('#coll_docs').empty();
         var escaper = $('<div></div>');
+        var table_html = `<table>
+                <tr>
+                    <th>Time</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Message</th>
+                </tr>`;
         for(var i = 0; i < response.data.length; i++){
             escaper[0].textContent = JSON.stringify(response.data[i], null, 4);
-            var inner_html = '<div class="col-xs-12 col-md-10 col-lg-10 no-side-pad"><pre class="code-block ' + docClass + '"><i class="fa fa-chevron-down code-block_expand"></i><code class="json">' + escaper[0].innerHTML + '</code></pre></div>';
-            inner_html += '<div class="col-md-2 col-lg-2 pad-bottom no-pad-right justifiedButtons">';
-            inner_html += '<div class="btn-group btn-group-justified justifiedButtons" role="group" aria-label="...">';
-            inner_html += '<a href="#" class="btn btn-danger btn-sm" onclick="deleteDoc(\'' + response.data[i]._id + '\')">' + response.deleteButton + '</a>';
-            inner_html += '<a href="' + $('#app_context').val() + '/app/' + conn_name + '/' + db_name + '/' + coll_name + '/' + response.data[i]._id + '" class="btn btn-info btn-sm">' + response.linkButton + '</a>';
-            inner_html += '<a href="' + $('#app_context').val() + '/app/' + conn_name + '/' + db_name + '/' + coll_name + '/edit/' + response.data[i]._id + '" class="btn btn-success btn-sm">' + response.editButton + '</a>';
-            inner_html += '</div></div>';
-            $('#coll_docs').append(inner_html);
+
+            var msg_collection = JSON.parse(escaper[0].innerHTML);
+            table_html += `<tr>
+                <td>${msg_collection.timestamp}</td>
+                <td>${msg_collection.from}</td>
+                <td>${msg_collection.to}</td>
+                <td>${msg_collection.message}</td>
+            </tr>`;
         };
+        table_html += '</table>';
+        $('#coll_docs').append(table_html);
         // Bind the DropDown Select For Fields
         var option = '';
         for(var x = 0; x < response.fields.length; x++){
